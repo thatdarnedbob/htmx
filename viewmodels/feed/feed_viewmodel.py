633500e40a6d6063b24f07108bd9fb7c4bb93d10
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-06-08 16:17:31 -0500

    infinite scroll

--- viewmodels/feed/feed_viewmodel.py
+++ viewmodels/feed/feed_viewmodel.py
@@ -6,7 +6,13 @@ from viewmodels.shared.viewmodelbase import ViewModelBase
 
 
 class FeedViewModel(ViewModelBase):
-    def __init__(self):
+    def __init__(self, page_size: int, page: int=1):
         super().__init__()
+        self.page_size = page_size
+        self.page = page
 
-        self.videos: List[Video] = video_service.all_videos()
+        all_videos = video_service.all_videos()
+        start = (page - 1)*page_size
+        end = start + page_size
+        self.videos: List[Video] = all_videos[start:end]
+        self.has_more_videos = len(all_videos) > end
